66501fe370be3263ec964e3964464e5d1eea3b4f
karolyna.assuncao <karolyna.assuncao@wapstore.com.br>
2024-01-02 17:55:44 -0300

    Finalização de projeto

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode () {
+  //troca de cores do tema
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // a função acima, funciona dessa forma:
+  // if (html.classList.contains('light')){
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  // }
+
+  // troca de imagem
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains('light')){
+    img.setAttribute('src', '/assets/avatar-light.png')
+  } else {
+    img.setAttribute("src", "/assets/avatar.png")
+  }
+}
